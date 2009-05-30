096b2a47dba93086bd5310f3488e43571b3ac1ac
Grzegorz Kazulak <grzegorz.kazulak@gmail.com>
2009-05-31 00:34:12 +0200

    first test

--- spec/flaker_spec.rb
+++ spec/flaker_spec.rb
@@ -4,8 +4,9 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 # http://rspec.info/
 describe "Place your specs here" do
   
-  it "find this spec in spec directory" do
-    violated "Be sure to write your specs"
+  it "should return object wrapper on fetch" do
+    flaker = Flaker::Base.new
+    flaker.fetch('type:user/login:dstranz/limit:2/source:photos/tag:bydgoszcz', {:source => 'photos'}).class == 'Flaker::ObjectWrapper'
   end
   
 end
