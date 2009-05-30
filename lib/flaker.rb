d4f267784b879b65a4ecd06cbd3aa89918d17cb0
Grzegorz Kazulak <grzegorz.kazulak@gmail.com>
2009-05-30 21:55:31 +0200

    error classes

--- lib/flaker.rb
+++ lib/flaker.rb
@@ -1,3 +1,23 @@
+require 'rubygems'
+
+gem 'httparty', '0.4.3'
+require 'httparty'
+
 module Flaker
   VERSION = '0.0.1'
+  
+  class FlakerError < StandardError
+    attr_reader :date
+    
+    def initialize(data)
+      @data = data
+      super
+    end
+  end
+  
+  class GeneralError      < FlakerError; end
+  class NotFoundError     < FlakerError; end
+  class UnauthorizedError < FlakerError; end
+  class Unavailable       < FlakerError; end
+  
 end
